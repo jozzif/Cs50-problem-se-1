20e06a858e09e012e77f4970f4483d9a99bf99bd
joseph amimo <josephamimo@icloud.com>
2026-05-01 07:48:49 +0300

    Refactor greeting function in Bank.py
    Refactored the greeting function to improve readability and maintain functionality. Added comments for clarity.

--- Bank.py
+++ Bank.py
@@ -1,32 +1,3 @@
-'''
-def main():
-    greeting()
-
-
-def greeting():
-    customer = input(
-        "Greet the bank teller: "
-    )  # If the greeting starts with “hello”, output $0. f the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
-    customer=customer.strip().lower()
-    if customer.startswith("hello"):
-        print("$0")
-    elif customer.startswith("h"):
-        print("$20")
-    else:
-        print("$100")
-
-
-main()
-
-
-'''
-
-
-
-
-
-
-
 description ='''
 In a file called bank.py,
 implement a program that prompts the user for a greeting. 
